58fe88a8e67bbc0f107289968abf9a8e692c78b5
Dan Stuart <dstuart@barracuda.com>
2014-04-24 15:54:12 -0400

    Added some templates and styling

--- flask_app.py
+++ flask_app.py
@@ -9,21 +9,17 @@ bootstrap = Bootstrap(app)
 app.secret_key = 'This is really unique and secret'
 
 @app.route('/')
-def hello_person():
-    return """
-        <title>Coldbrew Games</title>
-        <p>What is this, a game company for ants?</p>
-        <form method="POST" action="%s"><input name="person" /><input type="submit" value="Go!" /></form>
-        """ % (url_for('greet'),)
+def index():
+    return render_template('index.html')
+#     return """
+#         <title>Coldbrew Games</title>
+#         <p>What is this, a game company for ants?</p>
+#         <form method="POST" action="%s"><input name="person" /><input type="submit" value="Go!" /></form>
+#         """ % (url_for('greet'),)
 
-@app.route('/greet', methods=['POST'])
-def greet():
-    greeting = random.choice(["Hiya", "Hallo", "Hola", "Ola", "Salut", "Privet", "Konnichiwa", "Ni hao"])
-    return """
-        <title>Coldbrew Games</title>
-        <p>%s, %s!</p>
-        <p><a href="%s">Back to start</a></p>
-        """ % (greeting, request.form["person"], url_for('hello_person'))
+@app.route('/about')
+def about():
+    return render_template('about.html')
         
 def shutdown_server():
     func = request.environ.get('werkzeug.server.shutdown')
