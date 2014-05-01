e5b713664e0fe27302eac7910679eb684ca93ceb
Dan Stuart <dstuart@barracuda.com>
2014-05-01 12:52:37 -0400

    Config file reading fix

--- flask_app.py
+++ flask_app.py
@@ -7,51 +7,72 @@ import os
 import os.path
 
 app = Flask(__name__)
-# manager = Manager(app)
 bootstrap = Bootstrap(app)
 app.secret_key = 'This is really unique and secret'
 
-config = ConfigParser.SafeConfigParser()
-filepath = os.path.join(os.getcwd(), 'coldbrew.cfg')
-#config.read(filepath)
-config.read('coldbrew.cfg.bk')
+class IndexController(object):
+    
+    def __init__(self):
+        self.config = None
+    
+    def readConfig(self):
+        if not self.config:
+            self.config = ConfigParser.SafeConfigParser()
+            self.filepath = os.path.join(os.getcwd(), 'coldbrew.cfg')
+            self.config.read(self.filepath)
+    
+    def index(self):
+        self.readConfig()
+        return render_template('index.html')
+    
+    def about(self):
+        self.readConfig()
+        return render_template('about.html')
+    
+    def resume(self):
+        self.readConfig()
+        return render_template('resume.html')
+    
+    def eriu(self):
+        self.readConfig()
+        return render_template('eriu.html')
+    
+    def shutdown_server(self):
+        func = request.environ.get('werkzeug.server.shutdown')
+        if func is None:
+            raise RuntimeError('Not running with the Werkzeug Server')
+        func()
+    
+    def shutdown(self):
+        self.readConfig()
+        if self.config.getboolean('Controls', 'shutdown'):
+            self.shutdown_server()
+            return 'Server shutting down...'
+        return self.index()
+
+c = IndexController()
 
 @app.route('/')
 def index():
-
-    stuff = filepath + "<br>"
-    stuff += os.getcwd()
-
-    #file = open(filepath)
-    #stuff += file.read() + "<br>"
-    #file.close()
-
-    return stuff + render_template('index.html')
+    return c.index()
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return c.about()
 
 @app.route('/resume')
 def resume():
-    return render_template('resume.html')
+    return c.resume()
 
 @app.route('/eriu')
 def eriu():
-    return render_template('eriu.html')
-
-def shutdown_server():
-    func = request.environ.get('werkzeug.server.shutdown')
-    if func is None:
-        raise RuntimeError('Not running with the Werkzeug Server')
-    func()
+    return c.eriu()
 
 @app.route('/shutdown', methods=['GET'])
 def shutdown():
-    if config.getboolean('Controls', 'shutdown'):
-        shutdown_server()
-        return 'Server shutting down...'
-    return index()
+    return c.shutdown()
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
